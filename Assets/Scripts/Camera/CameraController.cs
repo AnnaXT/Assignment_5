2b0946197169da84c309a7c058d076db0c376ff2
Sergey <thankfulcon@gmail.com>
2023-02-27 23:39:02 -0500

    cought up with prev scene
    added new player

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+    public Vector3 Offset; 
+
+    void Update()
+    {
+        transform.position = Player.position + Offset;         
+    }
+}
