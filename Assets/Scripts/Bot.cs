af3c333042b477f3492231ca99980d87b630d81f
AnnaXT <annateng8000@gmail.com>
2023-02-28 18:46:37 -0500

    Updated Enemy Script & implemented Player Health
    Players can now take damage, the enemies will wait for a short while after touching the player

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -6,20 +6,46 @@ public class Bot : MonoBehaviour
 {
     UnityEngine.AI.NavMeshAgent _newNavMeshAgent;
     GameObject player;
+    private bool chase = true;
+    public float interval = 2f;
+    private float waitTime = 2f;
 
     void Start()
     {
         _newNavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(ChasePlayer());
+        waitTime = interval;
     }
 
     IEnumerator ChasePlayer()
     {
-        while (true)
+        while (chase)
         {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         _newNavMeshAgent.destination = player.transform.position;
         }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Player")){
+            other.GetComponent<PlayerHealth>().ChangeLifeVal(-1);
+            StopCoroutine("ChasePlayer");
+            chase = false;
+            
+        }
+
+    }
+
+    private void Update(){
+        if (chase == false){
+            waitTime -= Time.deltaTime;
+        }
+        if (waitTime <= 0){
+            waitTime = interval;
+            chase = true;
+            StartCoroutine("ChasePlayer");
+        }
+
     }
 }
--- Assets/Scripts/BotWithoutNavMesh.cs
+++ Assets/Scripts/BotWithoutNavMesh.cs
@@ -7,17 +7,42 @@ public class BotWithoutNavMesh : MonoBehaviour
     Rigidbody _rigidbody;
     private GameObject player;
     public float speed = 0.002f;
+
+    private bool chase = true;
+    public float interval = 2f;
+    private float waitTime = 2f;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        waitTime = interval;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform);
+        if (chase == false){
+            waitTime -= Time.deltaTime;
+        }
+        else{
+            transform.LookAt(player.transform);
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+        }
         
+        if (waitTime <= 0){
+            waitTime = interval;
+            chase = true;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Player")){
+            other.GetComponent<PlayerHealth>().ChangeLifeVal(-1);
+            chase = false;
+            
+        }
+
     }
 }
